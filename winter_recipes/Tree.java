ff02c32f3aa5e6deefdb42ebe481d0547b50cf46
League Student <league@iMac-9.attlocal.net>
2019-12-19 17:51:24 -0800

    :christmas_tree:

--- winter_recipes/Tree.java
+++ winter_recipes/Tree.java
@@ -16,12 +16,12 @@ public class Tree  {
   static double scale = 1.1;   
 	
   public static void main(String[] args) {
-
 	  // 1. Make the robot go fast
-
+rob.setSpeed(1000000000);
 	  
 	  // 2. Tell the robot to put its pen down
-
+rob.penDown();
+drawStar();
 	  // 3. Find the method called drawBranches below and complete its code
       drawBranches();
       
@@ -29,20 +29,28 @@ public class Tree  {
       drawTrunk();
       
 	  // 5. Find the method called drawStar below and complete its code
-      drawStar();
+   
 
 }
 
     
     static void drawBranches() {
         //  Change the color of the line the robot draws to green    
-
+rob.setPenColor(0, 255, 0);
         //  Make an int variable called turnAmount and set it to 175
-
+int turnAmount = 175;
         //  Set the robot's angle so it faces right
-
+rob.setAngle(90);
         //  Make a for loop that repeats 11 times and put the rest of the code that draws branches inside the for loop
-   
+   for (int i = 0; i < 200; i++) {
+	rob.move(treeWidth);
+	rob.turn(turnAmount);
+	treeWidth *= scale;
+	rob.move(treeWidth);
+	rob.turn(-turnAmount);
+	treeWidth *= scale;
+}
+   rob.move(treeWidth/2);
 
             // Move the robot the width of the tree (use the treeWidth variable from the top of this program)
     		
@@ -63,11 +71,12 @@ public class Tree  {
     
     static void drawTrunk() {
         //  Move the robot half the width of the tree
-    	
+    	rob.turn(90);
 
         // Set the robot's angle so that it is pointing straight down
-  
-
+  rob.setPenWidth(treeWidth/10);
+rob.setPenColor(90, 56, 10);
+rob.move(50);
         // Set the pen width to the tree width divided by 10
      
 
@@ -80,7 +89,11 @@ public class Tree  {
     
     static void drawStar() {
         // * Challenge: Write code to draw a red star on top of the tree. Hint: 144 degrees makes a star.
-    
+    rob.setPenColor(255, 0, 0);
+    for (int i = 0; i < 5; i++) {
+		rob.move(50);
+		rob.turn(144);
+	}
     }
 
 
